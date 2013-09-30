b1b6cf30cdd81229ce67c8e3952282858e873707
Eunsuk Kang <eskang@yellow-wasp>
2013-09-30 14:09:34 -0400

    Added cookie replay

--- lib/seculloy/case_studies/paywall/cookie_replay.rb
+++ lib/seculloy/case_studies/paywall/cookie_replay.rb
@@ -7,10 +7,17 @@ Seculloy::Dsl.view :CookieReplay do
   abstract data Str
   data Addr < Str
   data Name < Str
-  data Value < Str
-  data Pair[n: Name, v: Value] < Str
-  data URL[addr: Addr, queries: (set Pair)] < Str
+  data Pair[n: Name, v: Str] < Str
+  data AMap[entries: (set Pair)] < Str do
+    fun get[k: Name] do
+      {e : entries | e.n == k}.v
+    end
+      #entries.select {|p| p.n == k }
+  end
+
+  data URL[addr: Addr, queries: AMap] < Str
   data Cookie[domain: Addr, content: Pair] < Str
+  data NameCookie < Name
 
   trusted Client [
     cookies: Addr ** Cookie
@@ -25,22 +32,30 @@ Seculloy::Dsl.view :CookieReplay do
       sends { User::Display[cookies[addr]] }
     end
 
-    operation SendResponse[headers: (set Pair), body: Value] do 
+    operation SendResponse[headers: AMap, body: Str] do 
       sends { User::Display[body] }
     end
+    
+    operation Visit[url: URL] do
+      sends { Server::SendReq() { |op|
+          op.url == url and
+          op.headers.get(NameCookie) == cookies[url.addr]
+        }
+      }
+    end
   end
-
 
   trusted Server [
     session: URL ** Cookie
   ] do
-    operation SendReq[url: URL, headers: (set Pair)] do      
+    operation SendReq[url: URL, headers: AMap] do      
       sends { Client::SendResponse }
     end
   end
 
   mod User do
     operation Display[text: Str] do end
+    sends { Client::Visit }
   end
 
 end
