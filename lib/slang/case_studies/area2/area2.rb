4aead5a46601b68e468341c299fc00e0db4c4022
Eunsuk Kang <eskang@yellow-wasp>
2013-11-06 09:41:46 -0500

    Changes to area2.rb

--- lib/slang/case_studies/area2/area2.rb
+++ lib/slang/case_studies/area2/area2.rb
@@ -4,56 +4,61 @@ include Slang::Dsl
 
 Slang::Dsl.view :Area2 do
 
-  critical data Token
-  data FacultyID
-  data StudentID
-  critical data Profile[id: StudentID]
+  data UserID
+  critical data Token[encodes: UserID]  
+  abstract data UserType
+  one data TypeStudent < UserType
+  one data TypeAdmin < UserType
+  one data TypeFaculty < UserType
+  critical data Profile[id: UserID]
 
   trusted A2Site [
-    profiles: (dynamic StudentID ** Profile),
-    advisor: StudentID ** FacultyID,
-    tokens: StudentID ** Token
+     profiles: UserID ** Profile,
+     userType: UserID ** UserType
+#     profiles: UserID ** Profile                 
   ] do
     assumption {
-      # all s: StudentID, p: Profile do
-      #   p.id == s if profiles.contains?(s ** p) 
-      # end
-      profiles.all? {|si, p| p.id == si }
+      all uid: UserID, p: Profile do
+        uid == p.id if profiles.contains?(uid ** p) 
+      end
+    #   profiles.all? {|si, p| p.id == si }
     }
 
     creates Token
     creates Profile
 
-    op ViewProfile[id: StudentID, t: Token, ret: Profile] do
-      guard {  t == tokens[id] }
-      effects { ret == profiles[id] }
+    op ViewProfile[token: Token, ret: Profile] do
+#      guard { 
+      #  (userType[token.encodes] == TypeStudent and 
+      #   ret.id == token.encodes) or
+      #   userType[token.encodes] == Faculty or
+      #   userType[token.encodes] == Admin
+#      userType[token.encodes] == ret.id
+#      }
+#      effects { ret == profiles[token.encodes] }
     end
 
-    op EditProfile[id: StudentID, t: Token, newProfile: Profile] do
-      guard { t == tokens[id] }
-      effects { self.profiles = self.profiles + id ** newProfile }
-    end
   end
 
   trusted Faculty [
-    id: FacultyID
-  ] do
-    sends { A2Site::ViewProfile }
-    sends { A2Site::EditProfile }
-  end
-
-  mod Student [
-    id: StudentID,
+    id: UserID,
     token: Token
   ] do
     sends { A2Site::ViewProfile }
-    sends { A2Site::EditProfile }    
+  end
+
+  mod Student [
+    id: UserID,
+    token: Token
+  ] do
+    sends { A2Site::ViewProfile }
   end
 
   trusted Admin [
+    id: UserID,
+    token: Token
   ] do
     sends { A2Site::ViewProfile }
-    sends { A2Site::EditProfile }
   end
 
 end
